124850de774fce1843170dcdc8facd4e1f9dc9ab
rksgr <r_kjha@yahoo.com>
2022-02-04 13:11:26 +0530

    Added: A simple Servlet

---
+++ src/main/java/com/mySecondServlet.java
@@ -0,0 +1,19 @@
+package com;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/ServletTwo")
+public class mySecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3> My Servlet number two</h3>");
+        out.close();
+    }
+}

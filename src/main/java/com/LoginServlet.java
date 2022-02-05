ef368dd50601d8d31715caeeec7ab6761d3fcadd
rksgr <r_kjha@yahoo.com>
2022-02-05 09:00:40 +0530

    Refactored: doPost method to validate username and password

--- src/main/java/com/LoginServlet.java
+++ src/main/java/com/LoginServlet.java
@@ -18,7 +18,7 @@ import java.util.regex.Matcher;
 )
 public class LoginServlet extends HttpServlet {
 
-    /**UC 3: Extend the servlet to accept a valid name*/
+    /**UC 4: Extend the servlet to accept a valid name and password */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String userName = request.getParameter("user");
         String pwd = request.getParameter("pwd");
@@ -27,13 +27,37 @@ public class LoginServlet extends HttpServlet {
         Pattern pattern =Pattern.compile("[A-Z][a-zA-Z//s]{2,}");
         Matcher matcher = pattern.matcher(userName);
         boolean userNameValid =matcher.matches();
-        if(userNameValid){
+
+        // The password should have minimum 8 characters, should have at least 1 upper case,
+        // should have at least 1 numeric number and exactly 1 special character
+        int cnt_spl_char = 0;
+        boolean pwd_Correct = false;
+        Pattern pattern1 = Pattern.compile("[~!@#$%^&*()_+-]");
+        Matcher matcher1 = pattern1.matcher(pwd);
+        // count the number of special characters
+        while (matcher1.find()){
+            cnt_spl_char++;
+        }
+
+        // If only one special character exists
+        if (cnt_spl_char==1){
+            // Check for Minimum 8 characters out of which at least 1 upper case and 1 number
+            Pattern pattern2 = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^^&*)(_+-]{8,}");
+            Matcher matcher2 = pattern2.matcher(pwd);
+            pwd_Correct = matcher2.matches();
+        }
+        if(userNameValid  && pwd_Correct){
             request.setAttribute("user",userName);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
-        }else{
+        }else if(!userNameValid){
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
             out.println("<font color=red>User name  is invalid.<font>");
+            rd.include(request,response);
+        }else if(!pwd_Correct){
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Password  is invalid.<font>");
             rd.include(request,response);
         }
     }
